6c4bb7f6b0b11e078e49cd7e8e99b17e3b53e4c0
Natalya <sofazonia@gmail.com>
2024-11-07 18:17:47 -0500

    task 3.2

--- features/environment.py
+++ features/environment.py
@@ -2,27 +2,34 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.firefox.service import Service as FirefoxService
+from webdriver_manager.firefox import GeckoDriverManager
 
 from app.application import Application
 
 
-def browser_init(context):
+def browser_init(context, browser ='firefox'):
     """
     :param context: Behave context
     """
-    driver_path = ChromeDriverManager().install()
-    service = Service(driver_path)
 
-    chrome_options = Options()
-    chrome_options.add_argument("--headless")
-    chrome_options.add_argument("--disable-gpu")
-    chrome_options.add_argument("--window-size=1920x1080")
+    if browser == 'chrome':
+        driver_path = ChromeDriverManager().install()
+        service = Service(driver_path)
+        chrome_options = Options()
+        chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--disable-gpu")
+        chrome_options.add_argument("--window-size=1920x1080")
+        context.driver = webdriver.Chrome(service=service, options = chrome_options)
 
-    context.driver = webdriver.Chrome(service=service, options = chrome_options)
+    elif browser == 'firefox':
+        driver_path = GeckoDriverManager().install()
+        service = FirefoxService(driver_path)
+        context.driver = webdriver.Firefox(service=service)
+
 
     context.app = Application(context.driver)
-
-
+    context.driver.maximize_window()
     context.driver.implicitly_wait(4)
 
 
--- features/steps/test_cotactUs_page.py
+++ features/steps/test_cotactUs_page.py
@@ -1,27 +1,45 @@
 from behave import given, when, then
 from time import sleep
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
 
 @given ('open the main page')
 def open_main(context):
     context.app.main_page.open_main_page()
-    sleep(7)
+    sleep(8)
+
 
 @when('log in to the page')
 def log_in(context):
     context.app.login_page.login("natabudnik.w@gmail.com","Bober88")
+    sleep(8)
 
 
 @when('click on setting option')
 def click_settings(context):
     context.app.settings_page.click_settings()
+    sleep(8)
 
 @when('click on Contact us option')
 def click_contact_us(context):
     context.app.contactUs_page.click_contact_us()
+    sleep(8)
 
 @then('verify the right page opens')
 def verify_right_page(context):
-    assert context.driver.current_url == "https://soft.reelly.io/contact-us"
+
+    #WebDriverWait(context.driver, 20).until(EC.presence_of_element_located((By.XPATH, '/html/body/div[3]')))
+    WebDriverWait(context.driver, 30).until(EC.url_contains("contact-us"))
+
+    current_url = context.driver.current_url
+    print(f"Current URL: {current_url}")
+    #assert current_url == "https://soft.reelly.io/contact-us"
+    assert "contact-us" in current_url
+
+
+
 
 @then('verify there are at least 4 social media icons')
 def verify_social_media_icons(context):
